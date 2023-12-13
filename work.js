ffad46647e74bd15d7b9960555237c18bf2adf0c
CaptainSkiddow <dekkerrens@outlook.com>
2023-12-13 15:12:25 +0100

    x
    x

---
+++ work.js
@@ -0,0 +1,17 @@
+function welcome() {
+    console.log("Welcome")
+};
+
+welcome();
+
+let user_email = "dekkerrens@outlook.com"
+
+
+$(document).ready(function () {
+    $('#test').click(function () {
+        $("#test").hide();
+    });
+});
+
+
+
